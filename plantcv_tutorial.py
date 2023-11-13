176ab1e23c321291cad8154cd11ea15a3efb04ee
Insu Jeong <wjddlstn0614@naver.com>
2023-11-13 23:03:08 +0900

    FEAT : mask tutorial 추가

--- plantcv_tutorial.py
+++ plantcv_tutorial.py
@@ -1,6 +1,3 @@
-# plant cv tutorial
-# Author: Andrew Walker
-
 # Import libraries
 import os
 import numpy as np
@@ -16,3 +13,4 @@ img, path, filename = pcv.readimage(filename="img/original_image.jpg")
 s = pcv.rgb2gray_hsv(rgb_img=img, channel='s')
 s_thresh = pcv.threshold.binary(gray_img=s, threshold=85, object_type='light')
 
+s_mblur = pcv.median_blur(gray_img=s_thresh, ksize=3)
