60fb1ce70dce51cb930603f41d304f34eb010d8a
Insu Jeong <wjddlstn0614@naver.com>
2023-11-13 22:53:33 +0900

    first commit

---
+++ plantcv_tutorial.py
@@ -0,0 +1,18 @@
+# plant cv tutorial
+# Author: Andrew Walker
+
+# Import libraries
+import os
+import numpy as np
+import cv2
+from plantcv import plantcv as pcv
+pcv.params.debug = "plot"
+# pip install plantcv
+# pip install plantcv jupyterlab ipympl
+
+# load image
+img, path, filename = pcv.readimage(filename="img/original_image.jpg")
+
+s = pcv.rgb2gray_hsv(rgb_img=img, channel='s')
+s_thresh = pcv.threshold.binary(gray_img=s, threshold=85, object_type='light')
+
